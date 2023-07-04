10263a8d4d76b03f819789fcf5afef751072d6f4
Nikhil Raj <106947003+nik01575@users.noreply.github.com>
2023-07-04 13:27:09 +0530

    Add files via upload

---
+++ PPT Test 2/FifthQue.java
@@ -0,0 +1,13 @@
+public class FifthQue {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int myInteger = 42;
+        double myDouble = 3.14;
+        String myString = "Hello, world!";
+
+        // Print the values of the variables
+        System.out.println("Integer value: " + myInteger);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("String value: " + myString);
+    }
+}
---
+++ PPT Test 2/FirstQue.java
@@ -0,0 +1,16 @@
+public class FirstQue
+{
+    public static void main(String[] args) {
+
+        int num = 0;
+        // int num = 5;
+        // int num = -5;
+
+         if(num>0)
+            System.out.println(num + " is POSITIVE NUMBER.");
+        else if(num<0)
+            System.out.println(num + " is NEGATIVE NUMBER.");
+        else
+            System.out.println(num + " is a ZERO.");
+    }
+}
